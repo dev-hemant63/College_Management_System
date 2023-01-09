9f1bf9db2c8688346e62e5f87267d4ee09186aff
Amit Singh <np4652@gmail.com>
2023-01-09 17:11:28 +0530

    Done

--- JLNP_Project/AppCode/DL/ProcCommanMaster.cs
+++ JLNP_Project/AppCode/DL/ProcCommanMaster.cs
@@ -74,5 +74,122 @@ namespace JLNP_Project.AppCode.DL
             }
             return res;
         }
+        public ResponseStatus ProcAddAndUpdateProgram(ProgramMaster req)
+        {
+            var res = new ResponseStatus
+            {
+                statuscode = -1,
+                Msg = "Temp Error!"
+            };
+            string ProcName = "ProcAddAndUpdateProgram"; // Procedure name
+            SqlParameter[] param =
+            {
+                new SqlParameter("@Id",req.Id),
+                new SqlParameter("@Program",req.Program),
+                new SqlParameter("@ProgramType",req.ProgramType),
+                new SqlParameter("@ProgramDuration",req.ProgramDuration),
+                new SqlParameter("@LoginID",req.UserId)
+            };
+            try
+            {
+                var dt = _helper.ExcProc(ProcName, param);
+                if (dt.Rows.Count > 0)
+                {
+                    res.statuscode = Convert.ToInt32(dt.Rows[0]["Statuscode"]);
+                    res.Msg = Convert.ToString(dt.Rows[0]["Msg"]);
+                }
+            }
+            catch (Exception ex)
+            {
+                res.Msg = ex.Message;
+            }
+            return res;
+        }
+        public List<ProgramMaster> ProcGetProgram()
+        {
+            var res = new List<ProgramMaster>();
+            string ProcName = "Select * from tbl_Program order by Id Desc"; // Procedure name
+            try
+            {
+                var dt = _helper.ExcQueryWithoutParam(ProcName);
+                if (dt.Rows.Count > 0)
+                {
+                    foreach (DataRow dr in dt.Rows)
+                    {
+                        var model = new ProgramMaster
+                        {
+                            Id = Convert.ToInt32(dr["Id"]),
+                            Program = Convert.ToString(dr["Program"]),
+                            ProgramDuration = Convert.ToString(dr["ProgramDuration"]),
+                            EntryDate = Convert.ToString(dr["EntryDate"]),
+                            ProgramType = Convert.ToString(dr["ProgramType"]),
+                        };
+                        res.Add(model);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+            return res;
+        }
+        public ProgramMaster ProcEditProgram(int Id)
+        {
+            var res = new ProgramMaster();
+            string ProcName = "Select * from tbl_Program Where Id = @Id"; // Procedure name
+            SqlParameter[] param =
+            {
+                new SqlParameter("@Id",Id)
+            };
+            try
+            {
+                var dt = _helper.ExcQueryDT(ProcName, param);
+                if (dt.Rows.Count > 0)
+                {
+                    var model = new ProgramMaster
+                    {
+                        Id = Convert.ToInt32(dt.Rows[0]["Id"]),
+                        Program = Convert.ToString(dt.Rows[0]["Program"]),
+                        ProgramDuration = Convert.ToString(dt.Rows[0]["ProgramDuration"]),
+                        EntryDate = Convert.ToString(dt.Rows[0]["EntryDate"]),
+                        ProgramType = Convert.ToString(dt.Rows[0]["ProgramType"]),
+                    };
+                    res = model;
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+            return res;
+        }
+        public ResponseStatus ProcDeleteProgram(int Id)
+        {
+            var res = new ResponseStatus
+            {
+                statuscode = -1,
+                Msg = "Temp Error!"
+            };
+            string ProcName = "Delete from tbl_Program Where Id = @Id"; // Procedure name
+            SqlParameter[] param =
+            {
+                new SqlParameter("@Id",Id)
+            };
+            try
+            {
+                var Is = _helper.ExcQuery(ProcName, param);
+                if (Is)
+                {
+                    res.statuscode = 1;
+                    res.Msg = "Program Deleted Successfully!";
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+            return res;
+        }
     }
 }
