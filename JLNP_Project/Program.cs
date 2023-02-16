d3aaf974392aa9c22446e2d66b5afc4c45aeace3
Amit Singh <np4652@gmail.com>
2023-02-16 18:04:46 +0530

    Done

--- JLNP_Project/Program.cs
+++ JLNP_Project/Program.cs
@@ -37,5 +37,5 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Account}/{action=UsersLogin}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 app.Run();
